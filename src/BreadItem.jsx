9d4f656995617d49cbcbb964bf76f5f31d005bb5
rigood <rigood@naver.com>
2023-03-28 01:15:56 +0900

    잠금 시 스타일 변경

--- src/BreadItem.jsx
+++ src/BreadItem.jsx
@@ -35,52 +35,52 @@ function BreadItem({
       <Info>
         <MFD></MFD>
         <Quantity>
-          <button
-            type="button"
-            onClick={onDecrease}
-            disabled={count === 0 || isLock}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              width={36}
-              height={36}
-              strokeWidth={2}
-              stroke="white"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 12h-15"
-              />
-            </svg>
-          </button>
+          {!isLock && (
+            <button type="button" onClick={onDecrease} disabled={count === 0}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                width={36}
+                height={36}
+                strokeWidth={2}
+                stroke="white"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 12h-15"
+                />
+              </svg>
+            </button>
+          )}
           <input
             type="number"
             value={count}
             onChange={onChange}
-            disabled={isLock}
+            className={isLock && "red"}
           />
-          <button type="button" onClick={onIncrease} disabled={isLock}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              width={36}
-              height={36}
-              strokeWidth={2}
-              stroke="white"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </button>
+          {!isLock && (
+            <button type="button" onClick={onIncrease} disabled={isLock}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                width={36}
+                height={36}
+                strokeWidth={2}
+                stroke="white"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+            </button>
+          )}
         </Quantity>
       </Info>
     </Wrapper>
@@ -110,6 +110,9 @@ const Quantity = styled.div`
     text-align: center;
     font-size: 24px;
     border: none;
+  }
+  .red {
+    color: tomato;
   }
   button {
     width: 36px;
