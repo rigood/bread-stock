983f45dffc5af15de5095254ddf89220485759b9
rigood <rigood@naver.com>
2023-03-29 14:35:24 +0900

    시계 오류 수정

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -2,28 +2,31 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function Clock() {
-  let now = new Date();
-
-  const date = now.toLocaleDateString("en-US").slice(0, -5);
-  const day = "(" + now.toLocaleDateString("ko-KR", { weekday: "short" }) + ")";
-
-  const hours = now.getHours();
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-
-  const clockText = date + day + " " + hours + ":" + minutes + ":" + seconds;
-
-  const [clock, setClock] = useState(clockText);
+  const [now, setNow] = useState(new Date());
 
   useEffect(() => {
-    const Timer = setInterval(() => setClock(clockText), 1000);
+    const clock = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
 
     return () => {
-      clearInterval(Timer);
+      clearInterval(clock);
     };
-  }, [clockText]);
+  }, []);
 
-  return <Wrapper>{clock}</Wrapper>;
+  return (
+    <Wrapper>
+      <span>{now.toLocaleDateString("en-US").slice(0, -5)}</span>
+
+      <span>
+        {" (" + now.toLocaleDateString("ko-KR", { weekday: "short" }) + ") "}
+      </span>
+
+      <span>
+        {now.getHours() + ":" + String(now.getMinutes()).padStart(2, "0")}
+      </span>
+    </Wrapper>
+  );
 }
 
 export default Clock;
